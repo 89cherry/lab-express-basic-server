014d7be1517ad3d89f966052739cbbb6501a0b24
Diego Cerezo <diego.cerezo@gmail.com>
2024-05-20 12:23:55 +0200

    solved lab

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.static('public'))
+app.use(express.json())
 
 
+// routing
 
-// CREATE EXPRESS APP
-// Here you should create your Express app:
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/home.html`)
+})
+
+app.get('/api/projects', (req, res) => {
+    res.sendFile(`${__dirname}/data/projects.json`)
+})
+
+app.get('/api/articles', (req, res) => {
+    res.sendFile(`${__dirname}/data/articles.json`)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/views/not-found.html`)
+})
 
 
-
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-
-
-// ROUTES
-// Start defining your routes here:
-
-
-
-// START THE SERVER
-// Make your Express server listen on port 5005:
+// listen
+app.listen(5000, () => {
+    console.log('Servidor levantado')
+})
